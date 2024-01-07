6d187136c82695064243190858e14977631bb026
Huyh Huyhu <2509roblox@gmail.com>
2024-01-07 08:18:46 +0700

    add product part 1

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,15 +5,33 @@ import { CiShoppingCart } from "react-icons/ci"
 import { BsChevronCompactUp } from "react-icons/bs"
 import { BiSearch } from "react-icons/bi"
 import SearchBar from './SearchBar'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
     const [showProfile, setShowProfile] = useState<boolean>(false)
     const [showNav, setShowNav] = useState<boolean>(false)
-    // console.log(session?.user)
+    const { data: session } = useSession()
 
+    console.log(session?.user)
 
+    const SignOut = () => {
+        if (session && session.user) {
+            return (
+                <ul className='py-5 px-1 text-neutral-600'>
+                    <li className='hover:bg-gray-100 hover:text-neutral-900 px-5 py-2 cursor-pointer'>{session.user.name}</li>
+                    <li onClick={() => signOut()} className='whitespace-nowrap hover:text-red-600 px-5 py-2 cursor-pointer'>SignOut</li>
+                    <li className='whitespace-nowrap hover:bg-gray-100 hover:text-neutral-900 px-5 py-2 cursor-pointer'><a href="/addproduct">Add Product</a></li>
+                </ul>
+            )
+        }
+        return (
+            <ul>
+                <li onClick={() => signIn()} className='whitespace-nowrap hover:bg-gray-100 hover:text-neutral-900 px-5 py-2 cursor-pointer'>SignIn</li>
+            </ul>
+        )
+    }
     return (
         <div>
             <div className='flex items-center justify-between py-4 relative'>
@@ -32,6 +50,7 @@ const Navbar = (props: Props) => {
                     <div onClick={() => setShowProfile(!showProfile)} className='relative cursor-pointer'>
                         <img src="user.jpg" className='w-[35px] h-[35px] rounded-full object-cover' alt="" />
                         <div className={`absolute bg-white z-[2] rounded-lg shadow-lg ${showProfile ? "" : "hidden"}`}>
+                            <SignOut />
                         </div>
                     </div>
 
---
+++ app/lib/session.ts
@@ -0,0 +1,12 @@
+import { getServerSession } from "next-auth";
+import { options } from "@/app/api/auth/[...nextauth]/options"
+
+export function getSession() {
+    return getServerSession(options)
+}
+
+export async function getCurrentUser() {
+    const session = await getSession()
+    return session
+    // return session?.user.id
+}
